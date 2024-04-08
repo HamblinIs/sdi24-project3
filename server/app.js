9674d44643297eb061f6473187cd2069d0b94514
Isaac Hamblin <isaac.hamblin@gmail.com>
2024-04-08 14:23:11 -0600

    testing git stuff

--- server/app.js
+++ server/app.js
@@ -2,6 +2,12 @@ import express from 'express';
 const app =  express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/launches', (req, res) => res.send('All the launches'));
+
+app.post('/launches', (req, res) => res.send('Create a new launch'));
+
+app.get('/launches/:id', (req, res) => res.send('Specific Launch'));
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
