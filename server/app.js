baabbd7679f4d7a661eb4e14f13bc998e165f9ce
Isaac Hamblin <isaac.hamblin@gmail.com>
2024-04-09 13:10:43 -0600

    first pass at sign in/up endpoints

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express =  require('express');
+const cors = require("cors");
+
 const app =  express();
 const port = 8080;
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
@@ -16,5 +19,30 @@ app.get('/launches', async (req, res) => {
 app.post('/launches', (req, res) => res.status(201).send('Create a new launch'));
 
 app.get('/launches/:id', (req, res) => res.send('Specific Launch'));
+
+let testUserData = {
+    name: "Astro Boy",
+    address: "Nunya Way",
+    email: "astro@example.com",
+    username: "galaxy-traveler",
+    password: "secret-pass"
+}
+
+app.post('/auth/signup', (req, res) => {
+    let userData = req.body;
+    knex("user-account").insert({name: userData.name, address: userData.address, email: userData.email, username: userData.username, password: userData.password});
+    res.status(201).json({status: "Authenticated"});
+})
+
+app.post('/auth/signin', async (req, res) => {
+    let userData = req.body;
+    let dbData = {};
+    knex("user-account").select('*').where({username: userData.username})
+    .then(dbResp => {
+        if (dbResp.password === userData.password) {
+            res.status(201).json({status: "Authenticated"})
+        }
+    })
+})
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
