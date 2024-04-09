05fd1673ae5da8d41c32b151458edb2a435a4b9f
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-09 13:29:59 -0600

    Creates ContactUs page with comment box

--- ui-app/src/ContactUs.js
+++ ui-app/src/ContactUs.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CommentBox from './Components/CommentBox';
+import './Styling/ContactUs.css'
 
 
 const ContactUs = () => {
@@ -10,8 +11,13 @@ const ContactUs = () => {
 
   return (
     <>
-    <p>Hello</p>
+    <p className='contact'>Contact Us</p>
+    <p className='message'>
+      Got a question, concern, or just want to say hello? We're here to help! Our dedicated customer service team is available to assist you with any inquiries you may have. Simply fill out the form below, and we'll get back to you as soon as possible. 
+     Your satisfaction is our top priority, and we're committed to providing you with exceptional service every step of the way.</p>
+     <div className='commentbox'>
     <CommentBox />
+    </div>
     </>
   );
 }
