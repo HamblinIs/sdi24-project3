a2f7b00a024786271111e09a05540f51278c35f5
Carthelan <108813438+Carthelan@users.noreply.github.com>
2024-04-09 14:05:50 -0600

    Create 03_ticket.js

---
+++ server/03_ticket.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function(knex) {
+  // Deletes ALL existing entries
+  await knex('ticket').del()
+  await knex('ticket').insert([
+    {id: 1, purchasing_user: 1, launch: 10},
+    {id: 2, purchasing_user: 2, launch: 5},
+    {id: 3, purchasing_user: 3, launch: 3}
+  ]);
+};
