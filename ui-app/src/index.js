69ce5b7de19530862b2fdcd9297e543d2fcafd7b
VirtualVeteran <estheroswald001@yahoo.com>
2024-04-09 12:34:04 -0600

    Isaac fixed the browserrouter error

--- ui-app/src/index.js
+++ ui-app/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ 
+    <App />,
+ 
   document.getElementById('root')
 );
 
