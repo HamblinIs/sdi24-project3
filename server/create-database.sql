7349615f06e178e83dae6845ec775cd6d5564ae1
jbeyer15 <masterchef325@yahoo.com>
2024-04-10 14:18:22 -0600

    compose shenanigans

--- server/create-database.sql
+++
@@ -1,3 +0,0 @@
--- docker cp ./create-database.sql 751b378a4be1:/tmp/create-database.sql
--- ^^ docker copy command, use in a terminal which is focused on the server folder
-CREATE DATABASE space_database;
--- server/knexfile.js
+++ server/knexfile.js
@@ -6,13 +6,7 @@
 module.exports = {
   development: {
     client: "postgresql",
-    connection: {
-      host: "127.0.0.1",
-      password: "docker",
-      user: "postgres",
-      port: 5432,
-      database: 'space_database'
-    },
+    connection: process.env.DB_CONNECTION_STRING
   },
 
   // development: {
